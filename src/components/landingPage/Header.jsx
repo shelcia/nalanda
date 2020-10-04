af7d148af5807922c1ab8ff27d63e0731c7d3127
noob <shelcia1999@gmail.com>
2020-10-05 01:23:36 +0530

    update header

--- src/components/landingPage/Header.jsx
+++ src/components/landingPage/Header.jsx
@@ -1,15 +1,22 @@
 import React from "react";
-import bg from "../../assets/background.png";
+// import bg from "../../assets/background.png";
 import Navbar from "./Navbar";
 
 const Header = () => {
   return (
     <React.Fragment>
-      <div className="header border">
-        <img src={bg} alt="" className="img-fluid pt-5" />
-        <div className="pt-5">
-          <h1>ghcgchc</h1>
-          <h1>ghcgchc</h1>
+      <div className="header">
+        <div className="row">
+          <div className="col-sm-7">
+            <h2 className="title">Nalanda Coaching Centre</h2>
+            <p className="catchline">
+              Nalanda, the Institute that ensure your Success <br />
+              Ancient Nalanda is Known for learning your Art <br />
+              <b className="highlight">Kanchi Nalanda</b> is known for coaching
+              in competitive exam
+            </p>
+          </div>
+          <div className="col-sm-5"></div>
         </div>
       </div>
 
