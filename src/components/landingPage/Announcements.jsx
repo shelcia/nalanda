fb207c6a30c36c09285d12b36aec107cd76dbc2a
noob <shelcia1999@gmail.com>
2020-10-05 23:54:52 +0530

    updating announcements

--- src/components/landingPage/Announcements.jsx
+++ src/components/landingPage/Announcements.jsx
@@ -1,19 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Carousel from "react-elastic-carousel";
+import Bar from "../../assets/bar.png";
 
 const Announcements = () => {
+  console.log(window);
+  console.log(Carousel);
+  const [itemsToShow, setItemsToShow] = useState(3);
+  const announcements = {
+    items: [
+      { id: 1, title: "item #1" },
+      { id: 2, title: "item #2" },
+      { id: 3, title: "item #3" },
+      { id: 4, title: "item #4" },
+      { id: 5, title: "item #5" },
+    ],
+  };
+
+  useEffect(() => {
+    const size = window.innerWidth;
+    if (size < 900) {
+      setItemsToShow(2);
+    }
+    if (size < 600) {
+      setItemsToShow(1);
+    }
+  }, [itemsToShow]);
+
   return (
     <React.Fragment>
-      <div className="title text-center">Announcements</div>
-      <div className="container">
-        <div className="card-deck border">
-          <div className="card" style={{ height: "100px" }}>
-            rrr
-          </div>
-          <div className="card" style={{ height: "100px" }}>
-            rr
-          </div>
-          <div className="card" style={{ height: "100px" }}>
-            rr
+      <div className="mt-5" id="announcement">
+        <h2 className="title text-center">Announcements</h2>
+        <div className="text-center">
+          <img src={Bar} alt="bar" id="bar" />
+        </div>
+        <div className="container-fluid">
+          <div className="card-deck border">
+            <Carousel itemsToShow={itemsToShow}>
+              {announcements.items.map((item) => (
+                <div
+                  className="card"
+                  style={{ height: "200px", width: "250px" }}
+                  key={item.id}
+                >
+                  {item.title}
+                </div>
+              ))}
+            </Carousel>
           </div>
         </div>
       </div>
--- src/components/landingPage/Testimonials.jsx
+++ src/components/landingPage/Testimonials.jsx
@@ -1,19 +1,40 @@
 import React from "react";
+import Carousel from "react-elastic-carousel";
+import Bar from "../../assets/bar.png";
 
 const Testimonials = () => {
+  const isMobile = window.innerWidth < 480;
+  const showItems = isMobile ? 1 : 3;
+  const testimonials = {
+    items: [
+      { id: 1, title: `<div>hi </div>` },
+      { id: 2, title: "item #2" },
+      { id: 3, title: "item #3" },
+      { id: 4, title: "item #4" },
+      { id: 5, title: "item #5" },
+    ],
+  };
+
   return (
     <React.Fragment>
-      <div className="title text-center">Testimonials</div>
-      <div className="container">
-        <div className="card-deck border">
-          <div className="card" style={{ height: "100px" }}>
-            rrr
-          </div>
-          <div className="card" style={{ height: "100px" }}>
-            rr
-          </div>
-          <div className="card" style={{ height: "100px" }}>
-            rr
+      <div id="testimonials">
+        <div className="title text-center">Testimonials</div>
+        <div className="text-center">
+          <img src={Bar} alt="bar" id="bar" />
+        </div>
+        <div className="container-fluid">
+          <div className="card-deck">
+            <Carousel itemsToShow={showItems}>
+              {testimonials.items.map((item) => (
+                <div
+                  className="card"
+                  style={{ height: "200px", width: "250px" }}
+                  key={item.id}
+                >
+                  {item.title}
+                </div>
+              ))}
+            </Carousel>
           </div>
         </div>
       </div>
