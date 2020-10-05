db700fa816224c5828c02d3bfe265398b86309ca
noob <shelcia1999@gmail.com>
2020-10-05 11:15:46 +0530

    counter update

--- src/components/landingPage/Counters.jsx
+++ src/components/landingPage/Counters.jsx
@@ -3,16 +3,19 @@ import React from "react";
 const Counters = () => {
   return (
     <React.Fragment>
-      <div className="container">
+      <div className="container-fluid mt-5" id="counters">
         <div className="card-deck border">
-          <div className="card" style={{ height: "100px" }}>
-            rrr
+          <div className="card" id="tnpsc">
+            <h2 className="title">500</h2>
+            <p className="desc">Students cleared TNPSC</p>
           </div>
-          <div className="card" style={{ height: "100px" }}>
-            rr
+          <div className="card" id="tneb">
+            <h2 className="title">600</h2>
+            <p className="desc">Students cleared TNEB</p>
           </div>
-          <div className="card" style={{ height: "100px" }}>
-            rr
+          <div className="card" id="trb">
+            <h2 className="title">700</h2>
+            <p className="desc">Students cleared TRB</p>
           </div>
         </div>
       </div>
