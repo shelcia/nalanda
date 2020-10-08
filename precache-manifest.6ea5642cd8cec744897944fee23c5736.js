6bd37de8517a67df1a98d0774f02f921fc4e9cc4
noob <shelcia1999@gmail.com>
2020-10-08 22:12:31 +0530

    Updates

---
+++ precache-manifest.6ea5642cd8cec744897944fee23c5736.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e001bef580a6261f449a9faee9d16b2",
+    "url": "/nalanda/index.html"
+  },
+  {
+    "revision": "3ba1146eff07b6650b9f",
+    "url": "/nalanda/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "4be8d8b394babbbd83f0",
+    "url": "/nalanda/static/css/main.33e16f09.chunk.css"
+  },
+  {
+    "revision": "3ba1146eff07b6650b9f",
+    "url": "/nalanda/static/js/2.9a19058e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/nalanda/static/js/2.9a19058e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4be8d8b394babbbd83f0",
+    "url": "/nalanda/static/js/main.923a504c.chunk.js"
+  },
+  {
+    "revision": "9ee004a32b71abc1d312",
+    "url": "/nalanda/static/js/runtime-main.39a848df.js"
+  },
+  {
+    "revision": "17dc9c7452969da59a9598136dde317d",
+    "url": "/nalanda/static/media/announcementBlob.17dc9c74.png"
+  },
+  {
+    "revision": "51b2a10d09dd40cc2b28f3afc3db74f9",
+    "url": "/nalanda/static/media/background.51b2a10d.png"
+  },
+  {
+    "revision": "8ff909052640ccf0e1fb11c80bd6b3d3",
+    "url": "/nalanda/static/media/login_illustration.8ff90905.png"
+  },
+  {
+    "revision": "80b558d48800c59705e56cce947b15bf",
+    "url": "/nalanda/static/media/nalanda_institute.80b558d4.png"
+  },
+  {
+    "revision": "4d00644e6c97233ea577c6cd1a8a810a",
+    "url": "/nalanda/static/media/nalanda_logo.4d00644e.png"
+  },
+  {
+    "revision": "cddacb06e4e6bba90e23c79d96a56967",
+    "url": "/nalanda/static/media/testimonialBlob.cddacb06.png"
+  }
+]);
