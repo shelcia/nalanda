2413d8026316eb57caaf0a54d47bc421111f0bf2
noob <shelcia1999@gmail.com>
2020-10-19 01:53:53 +0530

    update style

--- src/components/Dashboard/Admin/AdminDashboard.jsx
+++ src/components/Dashboard/Admin/AdminDashboard.jsx
@@ -1,10 +1,47 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const AdminDashboard = () => {
   return (
     <React.Fragment>
-      <h1>fff</h1>
-      <h1>fff</h1>
+      <div id='admindashboard'>
+        <div className='container-fluid pl-0 pr-0'>
+          <nav
+            className='navbar navbar-expand-md bg-dark navbar-dark'
+            id='navbar-dashboard'>
+            <button
+              className='navbar-toggler'
+              type='button'
+              data-toggle='collapse'
+              data-target='#collapsibleNavbar'>
+              <span className='navbar-toggler-icon'></span>
+            </button>
+
+            <div className='collapse navbar-collapse' id='collapsibleNavbar'>
+              <ul className='navbar-nav'>
+                <li className='nav-item'>
+                  <NavLink className='nav-link' to='users'>
+                    Users
+                  </NavLink>
+                  <NavLink className='nav-link' to='/profile'>
+                    Link
+                  </NavLink>
+                </li>
+                <li className='nav-item'>
+                  <NavLink className='nav-link' to='/feedetails'>
+                    Link
+                  </NavLink>
+                </li>
+                <li className='nav-item'>
+                  <NavLink className='nav-link' to='/resources'>
+                    Link
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
