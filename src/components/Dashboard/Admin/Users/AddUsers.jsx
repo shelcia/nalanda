1527ff32a89980666332793e930910bfca7c1032
shelcia <shelcia1999@gmail.com>
2020-12-12 12:11:03 +0530

    update add users

--- src/components/Dashboard/Admin/Users/AddUsers.jsx
+++ src/components/Dashboard/Admin/Users/AddUsers.jsx
@@ -2,6 +2,8 @@ import React, { useRef } from "react";
 import { useHistory } from "react-router-dom";
 import Navbar from "../Navbar";
 import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const AddUser = () => {
   const history = useHistory();
@@ -13,30 +15,46 @@ const AddUser = () => {
   const headers = {
     // "auth-token": token,
     "Content-Type": "application/json",
-    Accept: "application/json",
   };
 
-  const addUser = async () => {
-    const url = `${process.env.REACT_APP_API_LINK}admin/login`;
+  const successNotify = (message) => toast.success(message);
+  const failedNotify = (message) => toast.error(message);
 
-    const body = {
+  const addUser = async (event) => {
+    event.preventDefault();
+    toast.warning("We are adding the user to the backend");
+
+    const response = {
       userId: userId.current.value,
       name: name.current.value,
       type: type.current.value,
       password: password.current.value,
     };
 
-    console.log(body);
+    const body = JSON.stringify(response);
+    const url = `${process.env.REACT_APP_API_LINK}admin/register`;
 
-    axios.post(url, {
-      headers,
-      body: body,
-    });
+    axios
+      .post(url, body, {
+        headers: headers,
+      })
+      .then((response) => {
+        console.log(response.data);
+        if (response.data.status === "200")
+          successNotify(response.data.message);
+        else if (response.data.status === "400")
+          failedNotify(response.data.message);
+      })
+      .catch((error) => {
+        console.log(error);
+        failedNotify(error);
+      });
   };
 
   return (
     <React.Fragment>
       <Navbar />
+      <ToastContainer />
       <div id="admindashboard">
         <div className="container">
           <table
@@ -103,8 +121,7 @@ const AddUser = () => {
               type="button"
               className="btn btn-primary ml-3"
               onClick={(event) => {
-                event.preventDefault();
-                addUser();
+                addUser(event);
               }}
             >
               Add User
