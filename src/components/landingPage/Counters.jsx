abcdf38a7f9b9b2dc896bf89e71ee831bdee52ae
shelcia <shelcia1999@gmail.com>
2021-01-07 01:01:24 +0530

    update

--- src/components/landingPage/Counters.jsx
+++ src/components/landingPage/Counters.jsx
@@ -1,31 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import CountUp from "react-countup";
 
 const Counters = () => {
+  const [countUp] = useState([
+    {
+      count: "500",
+      desc: "Students cleared TNPSC",
+      class: "bg-blue color-white",
+    },
+    {
+      count: "600",
+      desc: "Students cleared TNEB",
+      class: "bg-yellow color-darkBlue",
+    },
+    {
+      count: "700",
+      desc: "Students cleared TRB",
+      class: "bg-darkBlue color-yellow",
+    },
+  ]);
+
+  const className =
+    "card mx-2 d-flex justify-content-center align-items-center shadow-sm text-center border border-0 rounded-0";
+
   return (
     <React.Fragment>
       <div className="container-fluid mt-5" id="counters">
         <div className="card-deck px-4">
-          <div
-            className="card mx-2 d-flex justify-content-center align-items-center shodow text-center bg-blue color-white"
-            id="tnpsc"
-          >
-            <h2 className="title">500</h2>
-            <p className="desc">Students cleared TNPSC</p>
-          </div>
-          <div
-            className="card mx-2 d-flex justify-content-center align-items-center shodow text-center bg-yellow color-darkBlue"
-            id="tneb"
-          >
-            <h2 className="title">600</h2>
-            <p className="desc">Students cleared TNEB</p>
-          </div>
-          <div
-            className="card mx-2 d-flex justify-content-center align-items-center shodow text-center bg-darkBlue color-yellow"
-            id="trb"
-          >
-            <h2 className="title">700</h2>
-            <p className="desc">Students cleared TRB</p>
-          </div>
+          {countUp.map((count) => (
+            <div
+              className={`${className} ${count.class}`}
+              style={{ height: "200px" }}
+            >
+              <h2 className="title" style={{ fontSize: "4rem" }}>
+                <CountUp end={count.count} duration={5} />
+              </h2>
+              <p className="desc">{count.desc}</p>
+            </div>
+          ))}
         </div>
       </div>
     </React.Fragment>
