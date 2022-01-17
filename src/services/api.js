118b1b69945549d3d7f9459efcd5cc56c548546b
VMS-Backend <shelcia1999@gmail.com>
2022-01-17 06:55:18 +0530

    api update

--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,3 @@
 export const LOCALHOST_URL = "http://localhost:4050/api";
 
-export const HEROKU_BASE_URL = "https://hrt-backend.herokuapp.com/api";
+export const HEROKU_BASE_URL = "https://nalanda-backend.herokuapp.com/api";
